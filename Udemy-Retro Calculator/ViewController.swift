7a8f8849b0200cc1135e79c3b66e1e240af35b41
Cuong <vmc529440@gmail.com>
2017-02-20 11:10:31 +0700

    Finish autolayout, add button sound and put some handle error code

--- Udemy-Retro Calculator/ViewController.swift
+++ Udemy-Retro Calculator/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var buttonAudio : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Create a path for the audio file
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        print("this is \(path)")
+        // Create an URL because the path above is just returning String, we need to convert it in to url so IOS can read it
+        if path != nil {
+            let pathURL = URL.init(fileURLWithPath: path!)
+            // Sometime when we download music from the internet to play it, if it's not there or the internet connection is fail, you app will crash so we have to use a way to handle this error
+            do {
+                // Get the button ready to play by finding it path and setting it in the ready mode
+                try buttonAudio = AVAudioPlayer.init(contentsOf: pathURL)
+                buttonAudio.prepareToPlay()
+            } catch let error as NSError {
+                // If it fail, it will do this
+                print(error.debugDescription)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonDidPress(sender : UIButton){
+        playSounds()
     }
-
+    func playSounds(){
+        // Handle errors
+        if buttonAudio != nil {
+            // Stop button when its playing
+            if buttonAudio.isPlaying == true {
+                buttonAudio.stop()
+            }
+            buttonAudio.play()
+        }else{
+            print("error")
+        }
+    }
 
 }
 
